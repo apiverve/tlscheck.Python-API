03375eb5408c7d4526648d7ce72a546132c93556
APIVerve Team <hello@apiverve.com>
2025-04-23 11:27:09 -0500

    Bug Fixes

---
+++ src/apiverve_tlschecker/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+import os
+lib_folder = os.path.dirname(os.path.realpath(__file__))
+requirements_file = os.path.join(lib_folder, 'requirements.txt')
+install_requires = ["requests >= 2.25.1", "setuptools >= 56.0.0"]
+if os.path.exists(requirements_file):
+    with open(requirements_file, 'r') as f:
+        install_requires = f.read().splitlines()
+
+setup(
+    name='apiverve_tlschecker',
+    version='1.1.9',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=install_requires,
+    description='TLS Check is an API that inspects the TLS/SSL configuration of a server identified by its IP address. It reports supported protocols, cipher suites, and potential vulnerabilities.',
+    author='APIVerve',
+    author_email='hello@apiverve.com',
+    url='https://apiverve.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
